abe7411210c0ab6f12baad2844ee80cd6487051d
idilbyr <idil.bayar@sabanciuniv.edu>
2025-11-24 23:21:58 +0300

    fix the navigator roterer error

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
 
-// Renkler
 class AppColors {
-  static const Color primaryColor = Color(0xFF4CAF50); // Yeşil (Buzdolabı teması)
+  static const Color primaryColor = Color(0xFF4CAF50);
   static const Color accentColor = Color(0xFF81C784);
   static const Color errorColor = Color(0xFFD32F2F);
   static const Color backgroundColor = Color(0xFFF5F5F5);
   static const Color textColor = Color(0xFF212121);
 }
 
-// Yazı Stilleri
 class AppTextStyles {
-  // Custom Font: Roboto
   static const TextStyle headerStyle = TextStyle(
     fontSize: 24,
     fontWeight: FontWeight.bold,
     color: AppColors.textColor,
-    fontFamily: 'Roboto', 
+    fontFamily: 'Roboto',
   );
 
   static const TextStyle bodyStyle = TextStyle(
